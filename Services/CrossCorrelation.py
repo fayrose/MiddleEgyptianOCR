78ea51e9d560b2330d544df2b8c9f376268644a6
thomas horga <thomas.horga@gmail.com>
2019-11-27 21:00:56 -0500

    created cross correlation method

---
+++ Services/CrossCorrelation.py
@@ -0,0 +1,12 @@
+import os
+import random
+import json
+class CrossCorrelation:
+    
+    def __init__(self, char_img,chars_set_folder):
+        self.img = char_img
+        self.char_folder = chars_set_folder
+
+
+    def correlate(self):
+        #INSERT CORELATION CODE
