a9cbfbf90f407f92765a9ff19091109a0f607522
Lauren Rosenberg <laurenfay.rosenberg@mail.utoronto.ca>
2019-12-01 16:53:16 -0500

    Added recovery function, untestsed

--- MomentMatcher.py
+++ MomentMatcher.py
@@ -41,20 +41,25 @@ class Matcher:
                     best_candidate_moment = dist
                     best_candidate_name = glyph
 
-        return best_candidate_name
+        return best_candidate_name, best_candidate_moment
 
     def classify_entry(self, entry):
+        class_dict = {}
         if len(entry.gardiners) == 1 and len(entry.glyphs) == 1:
             entry.glyphs[0].set_classification(entry.gardiners[0])
             return True
 
-        
         # Classify each sign in entry
         classified = []
         for sign in entry.glyphs:
-            classification = self.classify(sign.image, entry.gardiners)
+            classification, dist = self.classify(sign.image, entry.gardiners)
             classified.append(classification)
             sign.set_classification(classification)
+
+            if classification in class_dict:
+                class_dict[classification].append((dist, sign))
+            else:
+                class_dict[classification] = [(dist, sign)]
         
         # Create count dict of classified signs and assert equal
         dict1 = Counter(entry.gardiners)
@@ -63,9 +68,39 @@ class Matcher:
 
         # If not equal, take sign that has been classified more than it should
         # and only take top k correct. Distribute other signs.
-        print("Deal with this situation.")
+        self.try_and_recover(entry, class_dict, dict1, dict2)
         return False
     
+    def try_and_recover(self, entry, class_dict, dict1 : Counter, dict2 : Counter):
+        # Partition dictionaries into over/under/correctly classified
+        over, under, same = {}, {}, {}
+        for dkey in dict1.keys():
+            d1, d2 = dict1[dkey], dict2.get(dkey, default=0)
+            if d1 > d2:
+                under[dkey] = [d1, d2]
+            elif d1 < d2:
+                over[dkey] = [d1, d2]
+            elif d1 == d2:
+                same[dkey] = d1
+        
+        # Distribute overclassified to underclassified
+        for dkey, tpl in over.items():
+            # Get distance of each in classified
+            # Take lowest d1-distances to keep
+            classed_sorted_by_dist = sorted(class_dict.get(dkey, default=0), key=lambda x: x[0])
+            to_reclassify = classed_sorted_by_dist[tpl[0]:]
+            
+            # For any distances higher than that, reclassify to under
+            for _, sign in to_reclassify:
+                out_class, _ = self.classify(sign.image, under.keys())
+                sign.set_classification(out_class)
+
+            # Update dictionaries
+                over[dkey][1] -= 1
+                under[dkey][1] += 1
+        classified = [sign.gardiner for sign in entry.glyphs]
+        print("Reconstruction successful: {0}".format(Counter(classified) == dict1))
+
     def classify_entries(self, entry_list):
         num = 0
         good_entries = []
