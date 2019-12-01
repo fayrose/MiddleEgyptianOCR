f9d19fe10a9a98902fdffe9362973a845d9d7cb5
Lauren Rosenberg <laurenfay.rosenberg@mail.utoronto.ca>
2019-12-01 18:47:39 -0500

    added special glyphs and comment needed impl.

--- SpecialGlyphs.py
+++ SpecialGlyphs.py
@@ -1,3 +1,4 @@
-tripleThreats = set(["Z2","Z3A","Z3"])
-electricPlug = set(["Z2B"])
+tripleThreats = set(["Z2","Z3A","Z3", "N33A", "N33AV", "M33", "M33A", "Z2C"])
+electricPlug = set(["Z2B"]) # need to add Z2A
 tripleHotDog = set(["N16"])
+# To Add: M39B, N19, N35A, Z4, Z4A, Z4B, U50
