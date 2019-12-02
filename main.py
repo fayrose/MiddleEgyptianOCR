b755de38ce147fbbde2df1587a6ef379ac40fd53
Lauren Rosenberg <laurenfay.rosenberg@mail.utoronto.ca>
2019-12-01 19:01:45 -0500

    push main

--- main.py
+++ main.py
@@ -12,25 +12,35 @@ import numpy as np
 
 def main():
 # C:\Users\Tom-H\Documents\CSC420\MiddleEgyptianOCR\Services\DataLoader.py
-    allEntries = []
     entry_img_folder = r"C:\Users\lfr2l\source\repos\DatasetGenerator\entry_images"
     data_json_path = r"C:\Users\lfr2l\source\repos\DatasetGenerator\DatasetGenerator\data.json"
     char_img_folder = r"C:\Users\lfr2l\source\repos\DatasetGenerator\character_images"
+    batches = [range(item, item + 100) if item != 2501 else range(item, 2569) for item in range(1, 2569, 100)] 
+
     dataLoader = DataLoader(entry_img_folder,data_json_path,char_img_folder)
-    image_path,sign_list,answer = dataLoader.load_entries_on_page(3)
-    p = 0
-    for i in range(len(image_path)):
-        entry = Entry(image_path[i])
-        entry.gardiners = sign_list[i]
-        entry.process_image()
-        allEntries.append(entry)
-        p += 1
-        
-    proc_acc, filtered = processing_accuracy(allEntries)
-    print("Processing Accuracy: {0}".format(proc_acc))
     gm = Matcher(char_img_folder)
-    class_rate, good_entries = gm.classify_entries(filtered)
-    print("Classification Accuracy: {0}".format(class_rate))
+    proc_list, class_list = [], []
+
+    for batch in batches:
+        allEntries = []
+        print("Batch - Pages {0} to {1}".format(batch.start, batch.stop - 1))
+        image_path,sign_list, answer = dataLoader.load_entries_in_range(batch)
+
+        for i in range(len(image_path)):
+            entry = Entry(image_path[i])
+            entry.gardiners = sign_list[i]
+            entry.process_image()
+            allEntries.append(entry)
+
+        proc_acc, filtered = processing_accuracy(allEntries)
+        print("Processing Accuracy: {0}".format(proc_acc))
+        class_rate, good_entries = gm.classify_entries(filtered)
+        print("Classification Accuracy: {0} \n".format(class_rate))
+        proc_list.append(proc_acc)
+        class_list.append(class_rate)
+
+    print("Processing accuracy over all batches: {0}".format(sum(proc_list) / len(proc_list)))
+    print("Classification accuracy over all batches: {0}".format(sum(class_list) / len(class_list)))
 
     # Now match good entries to their formatting
     #match(allEntries,char_img_folder)
